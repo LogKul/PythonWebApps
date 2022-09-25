6109866c413a96a05267a029c8f5232edb2dfe90
prilika1 <53357004+prilika1@users.noreply.github.com>
2022-09-25 15:17:08 -0600

    small fix to admin page

--- Project5-SuperheroDB/hero/admin.py
+++ Project5-SuperheroDB/hero/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Superhero
+from .models import *
 
 # Register your models here.
 admin.site.register(Superhero)
+admin.site.register(Strength)
+admin.site.register(Weakness)
