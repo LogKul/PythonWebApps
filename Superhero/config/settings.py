89df1a736e57f74d709604dcc2c1542d6763292c
prilika1 <53357004+prilika1@users.noreply.github.com>
2022-10-17 14:54:11 -0600

    Moving to postgres

--- Superhero/config/settings.py
+++ Superhero/config/settings.py
@@ -75,12 +75,40 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+POSTGRES_READY = (
+    POSTGRES_DB is not None
+    and POSTGRES_PASSWORD is not None
+    and POSTGRES_USER is not None
+    and POSTGRES_HOST is not None
+    and POSTGRES_PORT is not None
+)
+
+if POSTGRES_READY:
+    DATABASES = {
+        'default': {
+            # 'ENGINE': environ.get("DATABASE_ENGINE"),
+            # 'NAME': environ.get("DATABASE_NAME"),
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': POSTGRES_DB,
+            'USER': POSTGRES_USER,
+            'PASSWORD': POSTGRES_PASSWORD,
+            'HOST': POSTGRES_HOST,
+            'PORT': POSTGRES_PORT,
+        }
     }
-}
+    """
+    POSTGRES_DB = environ.get("POSTGRES_DB")
+    POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD")
+    POSTGRES_USER = environ.get("POSTGRES_USER")
+    POSTGRES_HOST = environ.get("POSTGRES_HOST")
+    POSTGRES_PORT = environ.get("POSTGRES_PORT")"""
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
